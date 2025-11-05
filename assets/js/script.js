c0ba4e4a78b7ce4d6270ed3d810ec1498df1e026
rozenrade <baillardquentin.pro@gmail.com>
2025-11-05 17:39:58 +0100

    fixed footer

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ const menu = document.getElementById("mobileMenu");
 btn.addEventListener("click", () => {
   menu.classList.toggle("translate-x-0"); // fait glisser le menu dans l’écran
   menu.classList.toggle("translate-x-full"); // remet hors écran
-  menu.classList.toggle("opacity-100"); // menu visible
+  menu.classList.toggle("opacity-[85%]"); // menu visible
   menu.classList.toggle("opacity-0"); // menu invisible
 });
 
